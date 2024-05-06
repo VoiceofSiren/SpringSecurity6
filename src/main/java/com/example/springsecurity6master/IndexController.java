6ed667c50b3a206df094aa20806daf6906f009d8
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-05-06 18:47:37 +0900

    feat: fundamental settings for SecurityConfig

--- src/main/java/com/example/springsecurity6master/IndexController.java
+++ src/main/java/com/example/springsecurity6master/IndexController.java
@@ -1,4 +1,12 @@
 package com.example.springsecurity6master;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
