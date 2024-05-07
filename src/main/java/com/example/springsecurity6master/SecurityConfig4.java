b9b3bca10c10d1f799cf361dcb764a4444e4f438
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-05-07 15:30:30 +0900

    feat: Authentication Process #04 - rememberMe()

--- src/main/java/com/example/springsecurity6master/SecurityConfig4.java
+++ src/main/java/com/example/springsecurity6master/SecurityConfig4.java
@@ -2,6 +2,7 @@ package com.example.springsecurity6master;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -10,16 +11,23 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-//@EnableWebSecurity
-//@Configuration
-public class SecurityConfig3 {
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig4 {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .httpBasic(basic -> basic
-                        .authenticationEntryPoint(new CustomAuthenticationEntryPorint()));
+                .formLogin(Customizer.withDefaults())
+                .rememberMe(rememberMe -> rememberMe
+//                        .alwaysRemember(true)
+                        .tokenValiditySeconds(3600)
+                        .userDetailsService(userDetailsService())
+                        .rememberMeParameter("rememberMe")
+                        .rememberMeCookieName("rememberMe")
+                        .key("security")
+                );
         return http.build();
     }
 
