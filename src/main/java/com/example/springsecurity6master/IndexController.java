8977b433667b9d25e334250e4b183e6c018409d7
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-05-09 12:28:07 +0900

    feat: Authentication Architecture #01 - Security Context(2)

--- src/main/java/com/example/springsecurity6master/IndexController.java
+++ src/main/java/com/example/springsecurity6master/IndexController.java
@@ -1,21 +1,28 @@
 package com.example.springsecurity6master;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
+
+    @Autowired
+    SecurityContextService securityContextService;
+
     @GetMapping("/")
-    public String index(String customParam) {
-        if (customParam != null){
-            return "customPage";
-        } else {
-            return "index";
-        }
+    public String index() {
+        SecurityContext securityContext = SecurityContextHolder.getContextHolderStrategy().getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        System.out.println("Controller - authentication = " + authentication);
+
+        securityContextService.securityContext();
+        return "index";
     }
 
     @GetMapping("/home")
