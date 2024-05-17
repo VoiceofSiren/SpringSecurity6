5c38bb8cf3deb9db770f83dd3441c81b5848eaa8
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-05-17 19:51:44 +0900

    feat: Session Management #02

--- src/main/java/com/example/springsecurity6master/_05_session_management/SecurityConfig2.java
+++ src/main/java/com/example/springsecurity6master/_05_session_management/SecurityConfig2.java
@@ -11,27 +11,20 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-//@EnableWebSecurity
-//@Configuration
-public class SecurityConfig1 {
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig2 {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/invalidSessionUrl", "/expiredUrl").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .sessionManagement(session -> session
-                        .invalidSessionUrl("/invalidSessionUrl")
-                        .maximumSessions(1)
-                        // default: false (이전 세션 종료)
-                        /*
-                        .maxSessionsPreventsLogin(true))
-                        */
-                        // true (추가 인증 시도 차단)
-                        .maxSessionsPreventsLogin(false)
-                        .expiredUrl("/expiredUrl"))
+                        .sessionFixation(sessionFixation -> sessionFixation
+                                /* default */
+                                .changeSessionId()))
         ;
         return http.build();
     }
@@ -41,4 +34,5 @@ public class SecurityConfig1 {
         UserDetails userDetails = User.withUsername("user").password("{noop}1111").roles("USER").build();
         return new InMemoryUserDetailsManager(userDetails);
     }
+
 }
