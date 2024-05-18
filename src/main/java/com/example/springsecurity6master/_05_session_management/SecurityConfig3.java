33ef2125dd66629bf8984b136b626c4d64b636be
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-05-18 11:17:56 +0900

    feat: Session Management #03

--- src/main/java/com/example/springsecurity6master/_05_session_management/SecurityConfig3.java
+++ src/main/java/com/example/springsecurity6master/_05_session_management/SecurityConfig3.java
@@ -5,15 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-//@EnableWebSecurity
-//@Configuration
-public class SecurityConfig2 {
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig3 {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -22,9 +23,7 @@ public class SecurityConfig2 {
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .sessionManagement(session -> session
-                        .sessionFixation(sessionFixation -> sessionFixation
-                                /* default */
-                                .changeSessionId()))
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
         ;
         return http.build();
     }
