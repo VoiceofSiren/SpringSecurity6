596c374a499d64280aecc20db693ecf1c3844670
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-06-02 20:33:36 +0900

    feat: Authorization Architecture #01 - Authority, Authorization Manager 01

--- src/main/java/com/example/springsecurity6master/_09_authorization_architecture/SecurityConfig1.java
+++ src/main/java/com/example/springsecurity6master/_09_authorization_architecture/SecurityConfig1.java
@@ -1,39 +1,50 @@
-package com.example.springsecurity6master._08_authorization_process;
+package com.example.springsecurity6master._09_authorization_architecture;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
-//@EnableWebSecurity
-//@EnableMethodSecurity
-//@Configuration
-public class SecurityConfig4_2 {
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig1 {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (webSecurity) -> {
+            webSecurity.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+        };
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/new_user").hasRole("USER")
+                        .requestMatchers("/new_db").access(new WebExpressionAuthorizationManager("hasRole('DB')"))
+                        .requestMatchers("/new_admin").hasAuthority("ROLE_ADMIN")
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 ;
-
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
-        UserDetails db = User.withUsername("manager").password("{noop}1111").roles("DB").build();
+        UserDetails user = User.withUsername("user").password("{noop}1111").authorities("ROLE_USER").build();
+        UserDetails db = User.withUsername("db").password("{noop}1111").roles("DB").build();
         UserDetails admin = User.withUsername("admin").password("{noop}1111").roles("ADMIN", "SECURE").build();
         return new InMemoryUserDetailsManager(user, db, admin);
     }
