1a2428b41ed17fcdf2bc3090e3bdbf7518fc05ef
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-06-03 19:45:32 +0900

    feat: Authorization Architecture #01 - Authority, Authorization Manager 02

--- src/main/java/com/example/springsecurity6master/_09_authorization_architecture/SecurityConfig2.java
+++ src/main/java/com/example/springsecurity6master/_09_authorization_architecture/SecurityConfig2.java
@@ -4,21 +4,24 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
-//@EnableWebSecurity
-//@Configuration
-public class SecurityConfig1 {
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+@Configuration
+public class SecurityConfig2 {
+
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
@@ -28,24 +31,25 @@ public class SecurityConfig1 {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/new_user").hasRole("USER")
-                        .requestMatchers("/new_db").access(new WebExpressionAuthorizationManager("hasRole('DB')"))
-                        .requestMatchers("/new_admin").hasAuthority("ROLE_ADMIN")
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/user").hasRole("USER")
+                        .requestMatchers("/db").access(new WebExpressionAuthorizationManager("hasRole('DB')"))
+                        .requestMatchers("/admin").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers("/secure").access(new CustomAuthorizationManager())
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
-                ;
+                .csrf(AbstractHttpConfigurer::disable);
+
         return http.build();
     }
-
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user").password("{noop}1111").authorities("ROLE_USER").build();
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
         UserDetails db = User.withUsername("db").password("{noop}1111").roles("DB").build();
-        UserDetails admin = User.withUsername("admin").password("{noop}1111").roles("ADMIN", "SECURE").build();
-        return new InMemoryUserDetailsManager(user, db, admin);
+        UserDetails admin = User.withUsername("admin").password("{noop}1111").roles("ADMIN","SECURE").build();
+        return  new InMemoryUserDetailsManager(user, db, admin);
     }
 }
